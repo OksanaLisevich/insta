4530928c8f114424647b1ef07062bee0333df613
OksanaLisevich <417ksenia@gmail.com>
2020-09-10 23:15:30 +0300

    ttt

--- index.php
+++ index.php
@@ -1,25 +1,30 @@
 <?php
 
-require("../vendor/autoload.php");
+require("/vendor/autoload.php");
+
+$proxy = 'set valid proxy';
+$searchedUsername = 'set username who are you looking for';
+$username = 'username of request account owner';
+$pass = 'password of request account owner';
+$newComment = 'some comment to add to post';
 
 $instagram = new \Instagram\Instagram();
 
 try {
 
 	 //Set the Proxy and Port
-    $instagram->setProxy('Hb7xHn:AfBew2@213.166.75.107:9024');
+    $instagram->setProxy($proxy);
 
     //Enable/Disable SSL Verification (Testing with Charles Proxy etc)
     $instagram->setVerifyPeer(false);
 
 
     //Login
-    $instagram->login('petrov52122', '05saa#1983');
+    $instagram->login($username, $pass);
 
 
-//var_dump($instagram); exit;
     //Find User by Username
-    $user = $instagram->getUserByUsername('ksenia417ksy');
+    $user = $instagram->getUserByUsername($searchedUsername);
 
     //Follow the User
     //$instagram->followUser($user);
@@ -45,27 +50,28 @@ try {
         $comments = $timelineFeedItem->getComments();
 
         //Which Filter did they use?
-        $filterType = $timelineFeedItem->getFilterType();
+        //$filterType = $timelineFeedItem->getFilterType();
 
         //Grab a list of Images for this Post (different sizes)
-        $images = $timelineFeedItem->getImageVersions2()->getCandidates();
+        //$images = $timelineFeedItem->getImageVersions2()->getCandidates();
 
         //Grab the URL of the first Photo in the list of Images for this Post
-        $photoUrl = $images[0]->getUrl();
+        //$photoUrl = $images[0]->getUrl();
 
-        echo sprintf("---------- Timeline Item ----------\n");
-        echo sprintf("User: %s [%s]\n", $user->getFullName(), $user->getUsername());
+        echo sprintf("---------- Timeline Item ----------<br>");
+        echo sprintf("User: %s [%s]<br>", $user->getFullName(), $user->getUsername());
        // echo sprintf("Caption: %s\n", $caption->getText());
-        echo sprintf("Like Count: %s\n", $likeCount);
-        echo sprintf("Comment Count: %s\n", $commentCount);
-        echo sprintf("Filter Type: %s\n", $filterType);
-        echo sprintf("Comments:\n", $commentCount);
+        echo sprintf("Like Count: %s<br>", $likeCount);
+        echo sprintf("Comment Count: %s<br>", $commentCount);
+        //echo sprintf("Filter Type: %s\n", $filterType);
+        //echo sprintf("Comments:\n", $commentCount);
 
         $mediaId = $timelineFeedItem->getId();
         var_dump($mediaId);
-        //var_dump();
-        //$likedMedia = $instagram->likeMedia($mediaId);
-        //$newComment = $instagram->commentOnMedia($mediaId, 'HI!)) It\'s my own \"like\"-test');
+        
+        $likedMedia = $instagram->likeMedia($mediaId);
+        
+        $newComment = $instagram->commentOnMedia($mediaId, $newComment);
         break;
 
     }
